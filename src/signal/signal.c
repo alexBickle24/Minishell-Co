a404ae45c0c32dcc9e2d83b47a7033abb2a3597f
vicalons <pachechecoo@gmail.com>
2025-05-05 20:40:00 +0200

    signal: ctrl+\ ignore

--- src/signal/signal.c
+++ src/signal/signal.c
@@ -1,10 +1,11 @@
 #include "minishell.h"
 
-int g_signal;
+int g_signal; //Variable global
 
 void    ft_signal_init(void)
 {
     g_signal = S_INIT;
+    signal(SIGQUIT, SIG_IGN); //Ignora la señal ctrl+'\'
     if (g_signal == S_INIT)
     {
         rl_on_new_line(); // Comienza una nueva linea
