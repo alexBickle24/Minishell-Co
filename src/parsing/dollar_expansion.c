2b5c282df379b2f4749940ea4cb3dfce69f5735f
alex <alexcarrillomc@outlook.com>
2025-08-27 21:09:45 +0200

    Añado funciones de estadp de comillas y trimeado

--- src/parsing/dollar_expansion.c
+++ src/parsing/dollar_expansion.c
@@ -1,20 +1,21 @@
 #include "../../inc/minishell.h"
 
-void	expand_and_cleanstr(char **str, t_msl *msl);
-void	dollar_expansion(char **str, int *i, size_t *len, t_msl *msl);
-void	replace_dollar(char **str, int *i, size_t *len, t_msl *msl);
-void	concatenate_strings(char **str, int *i, size_t *len, char *replace);
-void	concatenate_strings2(char **str, int *i, size_t *len, char *replace);
-void	expand_envvars(char **str, int *i, size_t *len, t_msl *msl);
+// void	expand_and_cleanstr(char **str, t_msl *msl);
+// void	dollar_expansion(char **str, int *i, size_t *len, t_msl *msl);
+// void	replace_dollar(char **str, int *i, size_t *len, t_msl *msl);
+// void	concatenate_strings(char **str, int *i, size_t *len, char *replace);
+// void	concatenate_strings2(char **str, int *i, size_t *len, char *replace);
+// void	expand_envvars(char **str, int *i, size_t *len, t_msl *msl);
 void	parser2(t_msl *msl);
 
-int g_signal = S_INIT;
+// int g_signal = S_INIT;
 
 void dollar_expansion(char **str, int *i, size_t *len, t_msl *msl)
 {
 	char	*dolim;
 	unsigned char	*idx;
 
+	// msl->parsing_utils->lexstat = D_QUOTES;//
 	idx = (unsigned char *)(*str);
 	dolim = msl->parsing_utils->dollar_lim;
 	if (dolim[idx[*i + 1]] >= 1 && dolim[idx[*i + 1]] < 5)//FUNCIONA
@@ -24,12 +25,14 @@ void dollar_expansion(char **str, int *i, size_t *len, t_msl *msl)
 	}
 	else if(dolim[idx[*i + 1]] == 5)//FUNCIONA
 	{
-		//caso fura de comillas
 		printf("Caso 5: dolim=5, char=%c\n", (*str)[*i + 1]);
-		ft_memmove(&((*str)[*i]), &((*str)[*i + 1]), *len - *i);
-		(*len) --;
-		//caso dentrol de comillas
-			// (*i)++;
+		if (msl->parsing_utils->lexstat == NO_QUOTES)
+		{
+			ft_memmove(&((*str)[*i]), &((*str)[*i + 1]), *len - *i);
+			(*len) --;
+		}
+		else
+			(*i)++;
 	}
 	else if (dolim[idx[*i + 1]] == 6 || dolim[idx[*i + 1]] == 0)//FUNCIONA
 	{
@@ -191,37 +194,37 @@ void parser2(t_msl *msl)
 }
 
 
-int main(int argc, char **argv, char **env)
-{
-	t_msl *msl;
-	char *line;//la linea en bruto
+// int main(int argc, char **argv, char **env)
+// {
+// 	t_msl *msl;
+// 	char *line;//la linea en bruto
 
-	(void)argv;//para que no se queje el compilador
-	if (argc != 1)
-		return (1);//si no hacemos el modo literal
-	minishell_init(&msl, env, argv);//inicamos la estrcutura de minishell y el manejados
-	while (1)
-	{
-		line = readline(PROMPT);
-		add_history(line);//exit tambien se mete al historial
-		msl->clean_line = line;
-		if (!msl->clean_line || ft_strncmp(msl->clean_line, "exit\0", 5) == 0)
-		{
-			ft_putstr_fd("exit\n", 2);
-			free(msl->clean_line);
-			msl->clean_line = NULL;//Esta linea es solo para poder printear los valores de la minishell
-			free(line);
-			break ;
-		}
-		// meter la linea en tester de variables
-		parser2(msl);
-		// free(line);
-		free(msl->clean_line);
-		msl->clean_line = NULL;//Esta linea es solo para poder printear los valores de la minishell
-		// printf("\n");
-		// print_msl(msl);
-	}
-	free_msl(&msl);
-	return (0);
-}
+// 	(void)argv;//para que no se queje el compilador
+// 	if (argc != 1)
+// 		return (1);//si no hacemos el modo literal
+// 	minishell_init(&msl, env, argv);//inicamos la estrcutura de minishell y el manejados
+// 	while (1)
+// 	{
+// 		line = readline(PROMPT);
+// 		add_history(line);//exit tambien se mete al historial
+// 		msl->clean_line = line;
+// 		if (!msl->clean_line || ft_strncmp(msl->clean_line, "exit\0", 5) == 0)
+// 		{
+// 			ft_putstr_fd("exit\n", 2);
+// 			free(msl->clean_line);
+// 			msl->clean_line = NULL;//Esta linea es solo para poder printear los valores de la minishell
+// 			free(line);
+// 			break ;
+// 		}
+// 		// meter la linea en tester de variables
+// 		parser2(msl);
+// 		// free(line);
+// 		free(msl->clean_line);
+// 		msl->clean_line = NULL;//Esta linea es solo para poder printear los valores de la minishell
+// 		// printf("\n");
+// 		// print_msl(msl);
+// 	}
+// 	free_msl(&msl);
+// 	return (0);
+// }
 
