917793e90a1b7223e6782098b029f78dcdc87002
Víctor Alonso <victor.alonso@altostratus.es>
2025-09-30 21:57:25 +0200

    feat: add ft_export incomplete

--- src/mains/main_builds.c
+++ src/mains/main_builds.c
@@ -259,6 +259,151 @@ int main(int argc, char **argv, char **env)
 	// 	tmp = tmp->next;
 	// }
 
+t_env	*ft_lstnew_env(char *id, char *value, int alloc)
+{
+	t_env	*new;
+
+	new = ft_calloc(1, sizeof(t_env));
+	new->id = id;
+	new->value = value;
+	if (alloc)
+	{
+		new->id = ft_strdup(id);
+		new->value = ft_strdup(value);
+		if (!new->id || !new->value)
+		{
+			ft_freeptr(new->id);
+			ft_freeptr(new->value);
+			ft_freeptr(new);
+			return (NULL);
+		}
+	}
+	new->next = NULL;
+	return (new);
+}
+
+t_env	*ft_sort_env(t_env *own_env)
+{
+	t_env	*tmp;
+	t_env	*swap;
+
+	tmp = own_env;
+	swap = ft_lstnew_env("", "", 0);
+	while (own_env->next != NULL)
+	{
+		if (own_env->next && ft_strcmp(own_env->id, own_env->next->id) > 0)
+		{
+			swap->id = own_env->id;
+			swap->value = own_env->value;
+			own_env->id = own_env->next->id;
+			own_env->value = own_env->next->value;
+			own_env->next->id = swap->id;
+			own_env->next->value = swap->value;
+			own_env = tmp;
+		}
+		else
+			own_env = own_env->next;
+	}
+	own_env = tmp;
+	ft_freeptr(swap);
+	return (tmp);
+}
+
+// Dudas con tmp->value[0]
+void	ft_print_env(t_env *own_env)
+{
+	t_env	*tmp;
+
+	tmp = own_env;
+	while(tmp)
+	{
+		if (!ft_strcmp(tmp->id, "_"))
+			printf("\r");
+		else if (tmp->value[0])
+			printf("declare -x %s\n", tmp->id);
+		else
+		{
+			printf("declare -x %s", tmp->id);
+			printf("=\"%s\n\"", tmp->value);
+		}
+		tmp = tmp->next;
+	}
+}
+
+int	ft_check_export(char *cmd)
+{
+	int	i;
+	char	*id;
+
+	i = 0;
+	id = ft_get_env_id(cmd);
+	if (ft_isdigit(id[i]))
+	{
+		printf("export: %s not a valid identifier\n", cmd);
+		ft_freeptr(id);
+		return (0);
+	}
+	while (id[i])
+	{
+		if (ft_isalnum(id[i]) || id[i] == '_')
+			i++;
+		else
+		{
+			printf("export: %s not a valid identifier\n", cmd);
+			ft_freeptr(id);
+			return (0);
+		}
+	}
+	ft_freeptr(id);
+	return (1);
+}
+
+void	ft_add_env(t_msl *msl, char *cmd)
+{
+	t_env	*tmp_env;
+	char	*id;
+	char	*value;
+
+	tmp_env = msl->own_env;
+	id = ft_get_env_id(cmd);
+	value = ft_get_env_value(tmp_env, id); //duda
+	printf("id: %s - value: %s\n", id, value);
+	// check value
+	// env = lst_new
+	// add back
+	ft_freeptr(id);
+	ft_freeptr(value);
+}
+
+/*
+ * Export es para añadir variables de entorno
+ * 
+ * Caso 1: export sin argc -> lista ordenada con declare
+ */
+void	ft_export(t_msl *msl)
+{
+	t_pcmds	*tmp;
+
+	printf("Start ft_export . . .\n");
+	if (ft_tokencounter(msl) == 1 && !ft_strcmp(msl->tocken->pcmds->cmd, "export"))
+	{
+		ft_print_env(ft_sort_env(msl->own_env));
+	}
+	else
+	{
+		tmp = msl->tocken->pcmds->next;
+		while (tmp)
+		{
+			printf("-------------------\n");
+			if (ft_check_export(tmp->cmd))
+				ft_add_env(msl, tmp->cmd);
+			else
+				return ;
+			tmp = tmp->next;
+		}
+	}
+}
+
 
 
 
@@ -281,8 +426,7 @@ void	interpreter_mode_builds(t_msl *msl, unsigned char *clean_line)
 	//msl:la cabecera (contiene el env y la variable sys)
 	//tocken->pcmds; los argumentos es decir lo que vas a parsear. Ej: linea-> echo palabra1 palabra2 palabra3
 
-	//ft_unset(msl);
-	ft_exit(msl);
+	ft_export(msl);
 
 	free_tockens(msl);//Para liberar los tockens cunado no tengo executer
 }
